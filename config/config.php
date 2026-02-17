52ac0b9db34a1b30926ada28d0a854a33bfa42c2
gitpronk-coder <gitpronk@gmail.com>
2026-02-17 10:34:38 +0200

    Create config/config.php for database and application configuration

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'database' => [
+        'host' => 'localhost',
+        'user' => 'your_username',
+        'password' => 'your_password',
+        'dbname' => 'your_database',
+    ],
+    'app' => [
+        'debug' => true,
+        'base_url' => 'http://yourapp.com',
+    ],
+];
